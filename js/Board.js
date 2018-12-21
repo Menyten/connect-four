12c537ecc77bc02c026c28a933841d5fc5195835
Joel <joel.pedersen97@gmail.com>
2018-12-21 23:37:59 +0100

    created evetnlisteners

--- js/Board.js
+++ js/Board.js
@@ -4,18 +4,33 @@ class Board {
     this.cols = 7;
     this.selector = selector;
     this.createBoard();
+    this.setupEventListeners();
   }
 
+  // The board should be made up of a 'slot' class
   createBoard() {
     const board = $(this.selector);
     for (let rows = 0; rows < this.rows; rows++) {
       const row = $('<div>').addClass('row');
       board.append(row);
       for (let cols = 0; cols < this.cols; cols++) {
-        const col = $('<div>').addClass('col empty');
+        const col = $('<div>')
+        .addClass('col empty')
+        .attr('data-col', cols)
+        .attr('data-row', rows);
         row.append(col);
       }
       console.log(board.html());
     }
   }
+
+  setupEventListeners() {
+    const board = $(this.selector);
+
+    board.on('mouseenter', '.col.empty', function () {
+      const col = $(this).data('col');
+      console.log(col);
+    });
+  }
+
 }
